445657866b739fc9f6e987fa23dba759cbceae99
GustavoDeveloper1 <devinvestidor02@gmail.com>
2020-07-27 16:48:35 -0300

    versão 1.0 do projeto

---
+++ assets/mail/contact_me.php
@@ -0,0 +1,26 @@
+<?php
+// Checar se esta vazio
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   echo "No arguments Provided!";
+   return false;
+   }
+   
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
+// criar o email e enviar o email
+$to = 'gustavodeveloper07@gmail.com'; //email que recebera a msg
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // email gerado automaticamente
+$headers .= "Reply-To: $email_address";   
+mail($to,$email_subject,$email_body,$headers);
+return true;         
+?>
